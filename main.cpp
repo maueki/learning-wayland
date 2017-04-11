eb26395e2efc89ef444d186bd8e2eec0b96676bf
Ueki Masaru <masaru.ueki@itage.co.jp>
2017-04-11 11:36:31 +0900

    event loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <cstdio>
+#include <wayland-server.h>
 
 int main(int argc, char *argv[])
 {
+    wl_display* display = wl_display_create();
+
     printf("hello, wayland\n");
+    wl_display_run(display);
+
     return 0;
 }
