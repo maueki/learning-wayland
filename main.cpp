d249540cb10123a2ced146cb9a6f43d4a561f89a
Ueki Masaru <masaru.ueki@itage.co.jp>
2017-04-11 11:51:23 +0900

    add socket

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <cstdio>
+#include <cstdlib>
+#include <error.h>
+#include <cassert>
 #include <wayland-server.h>
 
 int main(int argc, char *argv[])
 {
     wl_display* display = wl_display_create();
+
+    const char* socket_name = wl_display_add_socket_auto(display);
+    assert(socket_name);
 
     printf("hello, wayland\n");
     wl_display_run(display);
